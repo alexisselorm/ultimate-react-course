dbb655a6e34f741f3b191801571d2eb14b796e8e
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2025-05-02 22:49:00 +0300

    controlled forms in React

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useState } from "react";
 
 
  
@@ -27,21 +27,32 @@ function Logo(){
 }
 
 function Form(){
+  const [description, setDescription] = useState();
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e){
     e.preventDefault();
+
+    if(!description) return;
+
+    const newItem = {
+      description,quantity,packed:false,id:initialItems.length+1
+    }
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
     
   }
 
   return( 
   <form className='add-form' onSubmit={handleSubmit}>
     <h3>What do you need for your trip 😍</h3>
-    <select>
+    <select value={quantity} onChange={e=>setQuantity(+e.target.value)}>
       {Array.from({length:20},(_,i)=> i+1)
       .map(num=>(<option value={num} key={num}>{num}</option>)
       )}
     </select>
-    <input type="text" placeholder="item..."/>
+    <input type="text" placeholder="item..." value={description} onChange={(e)=>setDescription(e.target.value)}/>
     <button>Add</button>
   </form>
   )
