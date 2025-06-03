dc68902112e371909e0975f9893b6333530c1f2a
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2025-06-03 21:22:29 +0200

    Building the page for the routing project

---
+++ 11-worldwise/starter/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return ( 
+    <>
+    <AppNav/>
+    <div>...</div>
+    </>
+   );
+}
+
+export default AppLayout;
--- 11-worldwise/starter/src/App.jsx
+++ 11-worldwise/starter/src/App.jsx
@@ -3,19 +3,19 @@ import Product from '../pages/Product'
 import Pricing from '../pages/Pricing'
 import Homepage from '../pages/Homepage'
 import PageNotFound from '../pages/PageNotFound'
+import Login from '../pages/Login'
 
 export default function App(){
   return (
 
     <BrowserRouter>
     <Routes>
+      <Route path='/' element={<Homepage/>}/>
       <Route path='product' element={<Product/>}/>
       <Route path='pricing' element={<Pricing/>}/>
-      <Route path='/' element={<Homepage/>}/>
+      <Route path='login' element={<Login/>}/>
       <Route path='*' element={<PageNotFound/>}/>
-
-    </Routes>
-      
+    </Routes> 
       </BrowserRouter>
 
   )
