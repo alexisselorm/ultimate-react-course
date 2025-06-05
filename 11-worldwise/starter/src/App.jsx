d6e487fa19df6645147f3c9c523bc5bfa5c678c9
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2025-06-05 13:15:11 +0200

    navigating pages and passing data with react router

--- 11-worldwise/starter/src/App.jsx
+++ 11-worldwise/starter/src/App.jsx
@@ -1,11 +1,41 @@
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
-import Product from '../pages/Product'
-import Pricing from '../pages/Pricing'
-import Homepage from '../pages/Homepage'
-import PageNotFound from '../pages/PageNotFound'
-import Login from '../pages/Login'
+import {BrowserRouter,Navigate,Route,Routes} from 'react-router-dom'
+import Product from './pages/Product'
+import Pricing from './pages/Pricing'
+import Homepage from './pages/Homepage'
+import PageNotFound from './pages/PageNotFound'
+import Login from './pages/Login'
+import AppLayout from './pages/AppLayout'
+import CityList from './components/CityList'
+import City from './components/City'
+import Form from './components/Form'
+import { useEffect, useState } from 'react'
+import CountryList from './components/CountryList'
+
 
 export default function App(){
+
+  const [cities, setCities] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+      
+      async function getCities() {
+        try {
+          setLoading(true)
+          const res = await fetch('http://localhost:9000/cities')
+          const data = await res.json()
+          setCities(data)
+        } catch (error) {
+          console.log(error);
+        } finally{
+          setLoading(false);
+        }
+      }
+  
+      getCities();
+  
+    }, []);
+
   return (
 
     <BrowserRouter>
@@ -14,6 +44,13 @@ export default function App(){
       <Route path='product' element={<Product/>}/>
       <Route path='pricing' element={<Pricing/>}/>
       <Route path='login' element={<Login/>}/>
+      <Route path='app' element={<AppLayout/>}>
+        <Route index element={<Navigate replace to="cities"/>}/>
+        <Route path='cities' element={<CityList cities={cities} isLoading={loading} />} />
+        <Route path='cities/:id' element={<City/>}/>
+        <Route path='countries' element={<CountryList cities={cities} isLoading={loading} />} />
+        <Route path='form' element={<Form/>} />
+      </Route>
       <Route path='*' element={<PageNotFound/>}/>
     </Routes> 
       </BrowserRouter>
--- 11-worldwise/starter/src/components/City.jsx
+++ 11-worldwise/starter/src/components/City.jsx
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -9,6 +10,9 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+
+  const {id} = useParams()
+  console.log(id);
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -18,6 +22,8 @@ function City() {
   };
 
   const { cityName, emoji, date, notes } = currentCity;
+
+  
 
   return (
     <div className={styles.city}>
@@ -52,7 +58,7 @@ function City() {
       </div>
 
       <div>
-        <ButtonBack />
+        {/* <ButtonBack /> */}
       </div>
     </div>
   );
