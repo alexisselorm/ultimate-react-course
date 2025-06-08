818555a5ff8a9845ebe8e2fb498267a85ada526a
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2025-06-08 20:48:24 +0200

    Leaflet won't load. Fixing

--- 11-worldwise/starter/src/components/Map.jsx
+++ 11-worldwise/starter/src/components/Map.jsx
@@ -1,9 +1,13 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css'
+import {MapContainer,TileLayer,Marker,Popup} from 'react-leaflet';
+import { useState } from 'react';
+
 
 function Map() {
-
+  
   const [searchParams,setSearchParams]=useSearchParams();
+  const {mapPosition, setMapPosition}=useState([40,0])
   const lat=searchParams.get("lat")
   const lng=searchParams.get("lng")
 
@@ -11,11 +15,20 @@ function Map() {
 
 
 
-  return ( <div className={styles.mapContainer} onClick={()=>navigate("form")}>
-<h1>Map</h1>
-<h1>
-  Position:{lat},{lng}
-</h1>
+
+  return ( 
+  <div className={styles.mapContainer}>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
   </div> );
 }
 
