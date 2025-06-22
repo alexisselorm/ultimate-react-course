da9356bf46cd4d00732e03f5eaa102c0c0c4d68c
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2025-06-23 00:34:41 +0200

    Refactor App and Calculator components: optimize workouts with useMemo, enhance sound toggle functionality, and ensure proper memoization for performance improvements

--- 13-workout-timer/starter/src/Calculator.js
+++ 13-workout-timer/starter/src/Calculator.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
@@ -6,17 +6,40 @@ function Calculator({ workouts, allowSound }) {
   const [sets, setSets] = useState(3);
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
+  const [duration, setDuration] = useState(0);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
-  const mins = Math.floor(duration);
-  const seconds = (duration - mins) * 60;
 
-  const playSound = function () {
+
+
+  useEffect(() => {
+       const playSound = function () {
     if (!allowSound) return;
     const sound = new Audio(clickSound);
     sound.play();
   };
 
+    playSound();
+  }, [duration,allowSound]);
+
+  useEffect(() => {
+    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+
+  }, [number,sets,speed,durationBreak]);
+
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const mins = Math.floor(duration);
+  const seconds = (duration - mins) * 60;
+
+ 
+  function handleInc() {
+    setDuration((prev) => Math.floor(prev)+1)
+
+  }
+
+    function handleDec() {
+    setDuration((prev) => (prev>1 ? Math.ceil(prev)-1 : 0))
+
+  }
   return (
     <>
       <form>
@@ -66,16 +89,16 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
           {mins < 10 && '0'}
           {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
 }
 
-export default Calculator;
+export default memo(Calculator);
