73ee7075a8fd98789ec8aca41037652abdda485f
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2025-06-29 13:10:10 +0200

    feat: Refactor store setup to use configureStore from Redux Toolkit

--- 15-redux-intro/starter/src/store.js
+++ 15-redux-intro/starter/src/store.js
@@ -1,19 +1,13 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
-import thunk from 'redux-thunk'
 import customerReducer from "./features/customers/customerSlice";
-import {composeWithDevTools} from 'redux-devtools-extension'
+import {configureStore} from '@reduxjs/toolkit'
 
-
-
-
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
-});
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
+const store = configureStore({
+  reducer:{
+    account:accountReducer,
+    customer: customerReducer
+  }
+})
 export default store; 
 
 
