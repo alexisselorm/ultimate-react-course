bfce491b8532dcb20ff9094aa1a612a402194578
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2025-07-11 22:22:26 +0200

    Implement cart functionality with Redux; add item management, quantity updates, and clear cart feature; enhance UI components for better user experience

--- 16-fast-react-pizza/starter/src/features/cart/Cart.jsx
+++ 16-fast-react-pizza/starter/src/features/cart/Cart.jsx
@@ -2,36 +2,22 @@ import { Link } from 'react-router-dom';
 import Button from '../../ui/Button';
 import LinkButton from '../../ui/LinkButton';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart } from './cartSlice';
+import EmptyCart  from './EmptyCart'
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function Cart() {
-  const cart = fakeCart;
+  const cart = useSelector(getCart)
   const username = useSelector(state=>state.user.username)
 
+  const dispatch = useDispatch()
+  // console.log(object);
+
+  if (!cart.length) {
+    return <EmptyCart />
+    
+  }
 
   return (
     <div className='px-3 py-3 ' >
@@ -45,7 +31,7 @@ function Cart() {
 
       <div className='mt-6 space-x-2'>
         <Button type="primary" to="/order/new" >Order pizzas</Button>
-        <Button type="secondary" >Clear cart</Button>
+        <Button type="secondary" onClick={()=>dispatch(clearCart())} >Clear cart</Button>
       </div>
     </div>
   );
--- 16-fast-react-pizza/starter/src/features/cart/CartOverview.jsx
+++ 16-fast-react-pizza/starter/src/features/cart/CartOverview.jsx
@@ -1,11 +1,20 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
+
+  const numOfPizzas=useSelector(getTotalCartQuantity)
+  const pricefPizzas=useSelector(getTotalCartPrice)
+
+  if (!numOfPizzas) return null
+
   return (
     <div className="flex items-center justify-between px-4 py-4 text-sm uppercase bg-stone-800 text-stone-200 sm:px-6 md:text-base" >
       <p className="p-4 space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{numOfPizzas} pizzas</span>
+        <span>{formatCurrency(pricefPizzas)}</span>
       </p>
       <Link className="uppercase" to="/cart">Open cart &rarr;</Link>
     </div>
